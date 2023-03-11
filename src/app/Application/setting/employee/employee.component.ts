95ed9c06a6a452979994f7cfabc6a8c83944b1f7
jonmersha@gmail.com <jonmersha@gmail.com>
2023-03-11 09:51:48 +0300

    form created

--- src/app/Application/setting/employee/employee.component.ts
+++ src/app/Application/setting/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import Validation from 'src/app/utils/validation';
 
 export class User {
   public name!: string;
@@ -13,19 +15,66 @@ export class User {
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
-  model = new User();
-  hobbies: string[] = [
-    'Acrobatics',
-    'Acting',
-    'Animation',
-    'Astronomy',
-    'Baking',
-  ];
 
-  constructor(){}
+  empForm:FormGroup=new FormGroup({
 
-  onSubmit(form: any) {
-    console.log(form.value);
+  emp_id: new FormControl('',this.Vlist(2,6)),
+  firist_name: new FormControl('',this.Vlist(2,20)),
+  midle_name: new FormControl('',this.Vlist(2,20)),
+  last_name: new FormControl('',this.Vlist(2,20)),
+  employee_ou: new FormControl('',this.Vlist(2,20)),
+  job_title: new FormControl('',this.Vlist(2,20)),
+  is_auditor: new FormControl('',this.Vlist(2,20)),
+  email_id: new FormControl('',this.Vlist(2,20)),
+  phone_number: new FormControl('',this.Vlist(2,20)),
+  office_location: new FormControl('',this.Vlist(2,20)),
+  is_email_confirmed:new FormControl('',this.Vlist(2,20)),
+  organ_type: new FormControl('',this.Vlist(2,20))
+
+  })
+
+
+
+
+  // form: FormGroup = new FormGroup({
+  //   fullname: new FormControl(''),
+  //   username: new FormControl(''),
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   acceptTerms: new FormControl(false),
+  // });
+
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    
+  }
+  get form(): { [key: string]: AbstractControl } {
+    return this.empForm.controls;
   }
 
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.empForm.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.empForm.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.empForm.reset();
+  }
+
+  Vlist(minLen:number,maxLen:number){
+    return [Validators.required,Validators.minLength(minLen),Validators.maxLength(maxLen)]
+  }
+
+  
+ 
 }
--- src/app/Application/setting/ou/ou.component.ts
+++ src/app/Application/setting/ou/ou.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Validation from 'src/app/utils/validation';
 
 
 @Component({
@@ -9,20 +10,69 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 
 export class OuComponent implements OnInit{
-  userForm = new FormGroup({
-    userName: new FormControl('', [Validators.minLength(5), Validators.maxLength(10)]),
-    city: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(8)])	 
- });
-  constructor(){}
+
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
 
 
-  
+  submitted = false;
 
-  ngOnInit(){
-    
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
     
   }
-submitForm(){}
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 
   
  
